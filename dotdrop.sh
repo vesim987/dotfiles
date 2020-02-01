354ac23eb09824d476175bcd24faf2284c7d60ef
Vesim <vesim809@pm.me>
2020-02-01 19:47:10 +0100

    [misc] Add system configs

--- dotdrop.sh
+++ dotdrop.sh
@@ -10,7 +10,7 @@ if ! ${rl} "${0}" >/dev/null 2>&1; then
   rl="realpath"
 
   if ! hash ${rl}; then
-    echo "\"${rl}\" not found !" && exit 1
+    echo "\"${rl}\" not found!" && exit 1
   fi
 fi
 
@@ -22,10 +22,16 @@ cfg="${cur}/config.yaml"
 sub="dotdrop"
 
 # pivot
-cd "${cur}" || { echo "Folder \"${cur}\" doesn't exist, aborting." && exit; }
+cd "${cur}" || { echo "Directory \"${cur}\" doesn't exist, aborting." && exit 1; }
 # init/update the submodule
-git submodule update --init --recursive
+if [ "${DOTDROP_AUTOUPDATE-yes}" = yes ] ; then
+  git submodule update --init --recursive
+  git submodule update --remote dotdrop
+fi
 # launch dotdrop
-PYTHONPATH=dotdrop python3 -m dotdrop.dotdrop --cfg="${cfg}" "${args[@]}"
+PYTHONPATH=dotdrop python3 -m dotdrop.dotdrop "${args[@]}"
+ret="$?"
 # pivot back
-cd "${opwd}" || { echo "Folder \"${opwd}\" doesn't exist, aborting." && exit; }
+cd "${opwd}" || { echo "Directory \"${opwd}\" doesn't exist, aborting." && exit 1; }
+# exit with dotdrop exit code
+exit ${ret}
