b724bfd9ad978fd073ff9eb74bb55428969aa5aa
Vesim <vesim809@pm.me>
2020-08-24 17:26:16 +0200

    [sway] add missing files

--- dotfiles_home/config/sway/backlight.sh
+++ dotfiles_home/config/sway/backlight.sh
@@ -11,7 +11,7 @@ if [[ "${FOCUSED_OUTPUT_NAME}" = "eDP-1" ]]; then
 else
     for p in /sys/class/i2c-adapter/i2c-*/device/edid; do 
         if grep "${FOCUSED_OUTPUT_SERIAL}" "$p" 2>&1 1>/dev/null; then
-            DEVICE="ddcci$(echo "$p" | sed 's/.*i2c-\([0-9]\).*/\1/')"
+            DEVICE="sysfs/backlight/ddcci$(echo "$p" | sed 's/.*i2c-\([0-9]\).*/\1/')"
             break
         fi
     done
